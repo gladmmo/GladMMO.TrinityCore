3f38bb814a2461e50800073e0405375e5abcc364
HelloKitty <andrew.blakely@ymail.com>
2020-09-08 23:02:11 -0500

    Core/Player: Disable strict name check

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -8598,10 +8598,6 @@ ResponseCodes ObjectMgr::CheckPlayerName(std::string_view name, LocaleConstant l
     if (wname.size() < minName)
         return CHAR_NAME_TOO_SHORT;
 
-    uint32 strictMask = sWorld->getIntConfig(CONFIG_STRICT_PLAYER_NAMES);
-    if (!isValidString(wname, strictMask, false, create))
-        return CHAR_NAME_MIXED_LANGUAGES;
-
     return ValidateName(wname, locale);
 }
 
